818dff823dd91ebb48ff0e1feb5f4537ee01eb2e
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-03-17 10:07:56 +0000

    bug fix

--- src/index.js
+++ src/index.js
@@ -156,7 +156,7 @@ var IdentifiersJS = (function () {
 
     return {
         getResolver: factoryGetResolver,
-        unitTestResolver: testResolve()
+        unitTestResolver: testResolve
     };
 
 })();
