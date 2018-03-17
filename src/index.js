964842581f2266fdeb41afbcf898b95e99680ac2
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-03-17 03:18:30 +0000

    javascript entrypoint

---
+++ src/index.js
@@ -0,0 +1,8 @@
+/**
+ * Project: cloud-identifiersjs
+ * Timestamp: 2018-03-17 3:13
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ *
+ * This Javascript library implements services wrappers to access identifiers.org cloud API
+ */
