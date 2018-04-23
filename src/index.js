a5b40ab259abe18277831c42cfd781de8cccbae4
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-04-23 13:59:14 +0100

    ServiceResponseValidateRequest, TODO

--- src/index.js
+++ src/index.js
@@ -135,6 +135,10 @@ var IdentifiersJS = (function () {
 
     // --- (REGISTRY) Compact ID Resolution Services ---
     // Models
+    function ServiceResponseValidateRequest() {
+        // TODO
+    }
+
     function PrefixRequester(object) {
         this.name = "";
         this.email = "";
