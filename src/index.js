0420d59eabfff0e9213695acbc0388c292fe2c76
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-04-23 13:59:49 +0100

    inheritance call

--- src/index.js
+++ src/index.js
@@ -136,6 +136,7 @@ var IdentifiersJS = (function () {
     // --- (REGISTRY) Compact ID Resolution Services ---
     // Models
     function ServiceResponseValidateRequest() {
+        ServerResponse.call(this);
         // TODO
     }
 
